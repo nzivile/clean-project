8127aee1d028d4f8c26e1339df60d2d5b52f9a8f
Živilė Stankevičienė <n.zivile@yahoo.com>
2018-10-30 22:01:59 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Clean project</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
